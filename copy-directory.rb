7eaf84faf217f8dc665d9d96d8cd81e5fba0ce10
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-07 10:19:33 +0100

    modified to only print students with names shorter than 12 characters

--- copy-directory.rb
+++ copy-directory.rb
@@ -22,7 +22,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index| 
-    if student[:name][0] == "S" || student[:name][0] == "J"
+    if student[:name].length < 12
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
