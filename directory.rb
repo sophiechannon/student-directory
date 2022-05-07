f9fad13a33b3087a1b1d4c74337395fefa578446
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-07 09:52:32 +0100

    put student and cohorts into a hash

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
 #putting all students in an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked With of the West",
-  "Terminator",
-  "Freddy Kreuger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name:"Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked With of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Kreuger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 #printing students with iteration
-def print_students(names)
-  names.each { |student| puts student }
+def print(students)
+  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer(names)
@@ -26,6 +26,6 @@ def print_footer(names)
 end
 
 print_header
-print_students(students)
+print(students)
 print_footer(students)
 
