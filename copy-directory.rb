2be4c0117d567b32e435becee52d014ecd2133e9
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-07 10:16:07 +0100

    modified print to only print names starting with S or J

--- copy-directory.rb
+++ copy-directory.rb
@@ -21,7 +21,11 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index { |student, index| puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" }
+  students.each_with_index do |student, index| 
+    if student[:name][0] == "S" || student[:name][0] == "J"
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
 end
 
 def print_footer(names)
