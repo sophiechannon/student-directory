e9f72637f208deb926d16dd825dbd57f429da1ae
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-07 10:08:27 +0100

    made copy of directory for excercises

--- directory-copy.rb
+++ directory-copy.rb
@@ -32,21 +32,3 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
-
-
-
-=begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name:"Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked With of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
