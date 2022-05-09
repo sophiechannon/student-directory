09afb4daa9c73a477558a4f29af038c791a81772
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-09 11:43:08 +0100

    made pronoun switcher accessible anywhere in the program

--- copy-directory.rb
+++ copy-directory.rb
@@ -3,18 +3,29 @@ def input_students
 
   while true do
     puts "Enter next student name or press return twice to exit"
-    name = gets.chomp.capitalize
+    name = gets.chomp.split(" ").map!{|x| x.capitalize}.join(" ")
     break if name.empty?
+
     puts "Please enter their gender (M/F/NB)"
     gender = gets.chomp.upcase
-    puts "Please enter the student's height in cm"
+    if gender == "M"
+      gender = :male
+    elsif gender == "F"
+      gender = :female
+    else
+      gender = :neutral
+    end
+
+    puts "Please enter #{$pronoun[gender][:possessive]} height in cm"
     height = gets.chomp.to_i
-    puts "Please enter their hobbies"
+
+    puts "Please enter #{$pronoun[gender][:possessive]} hobbies, press return twice when done"
     hobbies = []
     while true do
       input = gets.chomp.downcase
       !input.empty? ? hobbies << input : break
     end
+    
     students << {name: name, cohort: :November, gender: gender, height: height, hobbies: hobbies }
     puts "Now we have #{students.count} students"
   end
@@ -22,26 +33,17 @@ def input_students
   students
 end
 
+$pronoun = {
+  male: {subject: "he", verb: "is", possessive: "his"},
+  female: {subject: "she", verb: "is", possessive: "her"},
+  neutral: {subject: "they", verb: "are", possessive: "their"}
+}
+
 def print_students(names)
   names.each_with_index do |student, index| 
-    subject_pronoun = ""
-    being_verb = "is"
-    possessive_pronoun = ""
-    case student[:gender] 
-    when "M"
-      subject_pronoun = "he"
-      possessive_pronoun = "his"
-    when "F"
-      subject_pronoun = "she"
-      possessive_pronoun = "her"
-    else
-      subject_pronoun = "they"
-      being_verb = "are"
-      possessive_pronoun = "their"
-    end
-
+    gender = student[:gender]
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    puts "#{subject_pronoun.capitalize} #{being_verb} #{student[:height]}cm tall and #{possessive_pronoun} hobbies are"
+    puts "#{$pronoun[gender][:subject].capitalize} #{$pronoun[gender][:verb]} #{student[:height]}cm tall and #{$pronoun[gender][:possessive]} hobbies are"
     puts "#{student[:hobbies].join(", ")}."
   end
 end
