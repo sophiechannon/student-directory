367a2515a4feaa045a7107db894faa97e355f0eb
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-09 14:07:38 +0100

    started interactive menu

--- copy-directory.rb
+++ copy-directory.rb
@@ -1,3 +1,12 @@
+def interactive_menu
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    user_input = gets.chomp
+  end
+end
+
 def input_students
   students = []
 
